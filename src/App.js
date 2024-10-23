a3f79ed13dbccd5f5327269a3c3c229335d292d4
MaksimBoyko05 <vladosmakasin1@gmail.com>
2024-10-23 12:10:50 +0300

    connected Redux to app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
-import Counter from './components/counter';
-import Tasks from './components/Tasks';
-
+import store from './store';
+import YourComponent from './YourComponent';
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        <Counter />
-        <Tasks />
-      </div>
+      <YourComponent />
     </Provider>
   );
 }
